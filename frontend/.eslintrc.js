0fbbe0a9347b48310b37ad0fcffd62f8444c4771
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-03-30 22:23:38 -0400

    create custom eslint config, install ts-jest

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    "react/react-in-jsx-scope": 0,
+    'no-console': 0,
+  },
+  settings: {
+    react: {
+        version: "detect"
+    }
+  },
+  ignorePatterns: [
+    "*.js",
+    'build',
+  ],
+};
