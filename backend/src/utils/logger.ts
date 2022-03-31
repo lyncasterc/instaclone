c2ae6ad3f063c8181dc665c3b4deee2994c6b347
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-03-31 17:06:42 -0400

    create logger.ts with loggers and logger helpers

---
+++ backend/src/utils/logger.ts
@@ -0,0 +1,38 @@
+const info = (...params: any[]) => {
+  console.log(...params);
+};
+
+const error = (...params: unknown[]) => {
+  console.error(...params);
+};
+
+// borrowed from: https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
+
+type ErrorWithMessage = {
+  message: string
+};
+
+const isErrorWithMessage = (maybeError: unknown): maybeError is ErrorWithMessage => (
+  typeof maybeError === 'object'
+    && maybeError !== null
+    && 'message' in maybeError
+    && typeof (maybeError as Record<string, unknown>).message === 'string'
+);
+
+function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
+  if (isErrorWithMessage(maybeError)) return maybeError;
+
+  try {
+    return new Error(JSON.stringify(maybeError));
+  } catch {
+    return new Error(String(maybeError));
+  }
+}
+
+const getErrorMessage = (maybeError: unknown) => toErrorWithMessage(maybeError).message;
+
+module.exports = {
+  info,
+  error,
+  getErrorMessage,
+};
