77c3c98170eb348a3cb08013901661d5183832f0
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-01 18:07:05 -0400

    remove _id from post model

--- backend/src/mongo/models/post.ts
+++ backend/src/mongo/models/post.ts
@@ -27,4 +27,12 @@ const postSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
+postSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 export default mongoose.model('Post', postSchema);
