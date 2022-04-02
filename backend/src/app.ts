71b5426332a1d87fd3fb7fd1eb0bd190b641d2fb
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-02 16:07:27 -0400

    Plug in the userRouter middleware in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import mongodbConnect from './mongo';
 import config from './utils/config';
+import userRouter from './routes/users';
 
 const { NODE_ENV } = process.env;
 const app = express();
 
-app.use(express.json());
-app.use(express.static('build'));
-app.use(cors());
-
 if (NODE_ENV === 'development') {
   mongodbConnect(config.DEV_MONGODB_URI!);
 }
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use('/api/users', userRouter);
 
 export default app;
