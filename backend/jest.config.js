0fafc2b2bc0c687237b260ebb234962ccb77684f
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-02 15:14:09 -0400

    remove regex for matching any file in /test
    now, only files with .test will be run when running npm test.

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
 };
