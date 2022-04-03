055580f69ddfb12729df28b4a293bf6277be922a
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-02 22:32:19 -0400

    Create getUserById in the user service

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,12 +1,39 @@
+import bcyrpt from 'bcrypt';
 import { User } from '../mongo';
+import { NewUser } from '../types';
 
 const getUsers = async () => {
+  // TODO: figure out if post id is populated by default.
+  // TODO: decide which are and populate the fields that should be populated.
   const users = await User.find({})
     .populate('posts', { image: 1 });
 
   return users;
 };
 
+const getUserById = async (id: string) => {
+  const user = await User.findById(id);
+  return user;
+};
+
+const addUser = async (user: NewUser) => {
+  const existingUser = await User.findOne({ username: user.username });
+  if (existingUser) throw new Error('That username is already taken!');
+  if (user.password.length < 5) throw new Error('Password is too short!');
+
+  const passwordHash = await bcyrpt.hash(user.password, 10);
+
+  const newUser = new User({
+    ...user,
+    passwordHash,
+  });
+
+  const savedNewUser = await newUser.save();
+  return savedNewUser;
+};
+
 export default {
   getUsers,
+  getUserById,
+  addUser,
 };
