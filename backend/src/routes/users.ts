1f431cf69365ee1a4f8cc89d922eab1176048c35
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-02 22:29:59 -0400

    Create routes for getting one user, new user

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,10 +1,34 @@
 import express from 'express';
 import userService from '../services/userService';
+import fieldParsers from '../utils/fieldParsers';
+import logger from '../utils/logger';
 
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
   res.send(await userService.getUsers());
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const user = await userService.getUserById(id);
+    if (!user) return res.status(404).end();
+    return res.send(user);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const newUser = fieldParsers.toNewUser(req.body);
+    const savedNewUser = await userService.addUser(newUser);
+    res.status(201).send(savedNewUser);
+  } catch (error) {
+    res.status(400).send({ error: logger.getErrorMessage(error) });
+  }
 });
 
 export default router;
