3f65530969d72c9240960e4d3286a388c4bf2756
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-02 22:31:33 -0400

    Add fieldParsers.ts
    This file contains type guards and helper functions to proof requests

---
+++ backend/src/utils/fieldParsers.ts
@@ -0,0 +1,35 @@
+import { NewUser } from '../types';
+
+const isString = (text: unknown): text is string => typeof text === 'string' || text instanceof String;
+
+const parseStringField = (param: unknown, fieldKey: string): string => {
+  if (!param || !isString(param)) {
+    throw new Error(`incorrect or missing ${fieldKey}`);
+  }
+
+  return param;
+};
+
+interface NewUserUnknownFields {
+  fullName: unknown,
+  email: unknown,
+  username: unknown,
+  password: unknown,
+}
+
+const toNewUser = ({
+  fullName, email, username, password,
+}: NewUserUnknownFields): NewUser => {
+  const newUser: NewUser = {
+    fullName: parseStringField(fullName, 'full name'),
+    email: parseStringField(email, 'email'),
+    username: parseStringField(username, 'username'),
+    password: parseStringField(password, 'password'),
+  };
+
+  return newUser;
+};
+
+export default {
+  toNewUser,
+};
