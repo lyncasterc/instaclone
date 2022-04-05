1b9ababf75fd495aa1ada650f911788704bb1780
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-05 18:39:31 -0400

    Add types for all models

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,7 +1,47 @@
+// TODO: do the reference fields need to be of type string or their actual types?
+
+export interface Comment {
+  id: string,
+  post: string, // ref
+  comment: string,
+  author: string, // ref -> User
+  parentComment?: string, // ref
+  replies?: string[] // ref -> Comment
+}
+
+export interface Post {
+  id: string,
+  creator: string, // ref -> User
+  caption?: string,
+  image: string,
+  comments?: string[], // ref
+  likes?: string[], // ref -> User
+}
+
+export interface User {
+  id: string,
+  fullName: string,
+  username: string,
+  email: string,
+  passwordHash: string,
+  image?: string,
+  posts?: string[], // ref
+  followers?: string[], // ref -> User
+  following?: string[], //  ref -> User
+}
+
 export interface NewUser {
   fullName: string,
   username: string,
   email: string,
   password: string,
+  image?: string,
+}
+
+export interface ProofedUpdatedUser {
+  fullName?: string,
+  username?: string,
+  email?: string,
+  password?: string,
   image?: string,
 }
