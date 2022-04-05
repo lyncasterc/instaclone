932fdb3c12f443b8260a7e831b2640a57b5b0935
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-05 18:44:53 -0400

    Add the Cloudinary env variables to config.ts

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -1,8 +1,17 @@
 require('dotenv').config();
 
-const { DEV_MONGODB_URI, PORT } = process.env;
+const {
+  DEV_MONGODB_URI,
+  PORT,
+  CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+} = process.env;
 
 export default {
   DEV_MONGODB_URI,
   PORT,
+  CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
 };
