0204b0c26a19e655cd71fbbfd6fd0925477b33a2
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-05 23:42:07 -0400

    Use the login router in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import mongodbConnect from './mongo';
 import config from './utils/config';
 import userRouter from './routes/users';
+import loginRouter from './routes/login';
 
 const { NODE_ENV } = process.env;
 const app = express();
@@ -16,5 +17,6 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 export default app;
