c341240e6f34a6b38bf426a6b842f7459e9aec37
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-05 23:39:46 -0400

    Write createTestUser func for testing login router

--- backend/test/test-helpers.ts
+++ backend/test/test-helpers.ts
@@ -1,10 +1,25 @@
+import bcrypt from 'bcrypt';
 import { User } from '../src/mongo';
+import { NewUser } from '../src/types';
 
 const usersInDB = async () => {
   const users = await User.find({});
   return users.map((user) => user.toJSON());
 };
 
+const createTestUser = async (user: NewUser) => {
+  const passwordHash = await bcrypt.hash(user.password, 10);
+  const testUser = new User({
+    fullName: user.fullName,
+    username: user.username,
+    email: user.email,
+    passwordHash,
+  });
+  const savedUser = (await testUser.save()).toJSON();
+  return savedUser;
+};
+
 export default {
   usersInDB,
+  createTestUser,
 };
