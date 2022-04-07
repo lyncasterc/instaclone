2331a98c0f3027b0b54f305ef300257760202268
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-06 22:02:36 -0400

    Refactor

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,7 @@ import mongodbConnect from './mongo';
 import config from './utils/config';
 import userRouter from './routes/users';
 import loginRouter from './routes/login';
-import middleware from './utils/middleware';
+import { errorHandler } from './utils/middleware';
 
 const { NODE_ENV } = process.env;
 const app = express();
@@ -18,9 +18,8 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use(middleware.tokenExtractor);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
 
-app.use(middleware.errorHandler);
+app.use(errorHandler);
 export default app;
