8e4e851b7345413ea277ce77d0e2330db45f3ac9
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-07 23:21:44 -0400

    Write a post route for the post router

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import postService from '../services/post-service';
 import { authenticator } from '../utils/middleware';
+import fieldParsers from '../utils/field-parsers';
+import logger from '../utils/logger';
+import cloudinary from '../utils/cloudinary';
 
 const router = express.Router();
 
@@ -9,5 +12,32 @@ router.get('/:id', authenticator(), async (req, res) => {
   if (!post) return res.status(404).end();
   return res.send(post);
 });
+
+router.post('/', authenticator(), async (req, res, next) => {
+  let post;
+
+  try {
+    post = fieldParsers.proofPostFields(req.body);
+  } catch (error) {
+    return res.status(400).send({ error: logger.getErrorMessage(error) });
+  }
+
+  try {
+    const imageUrl = await cloudinary.upload(post.image);
+    post.image = imageUrl;
+  } catch (error) {
+    return res.status(500).send({ error: 'Something went wrong uploading the photo!' });
+  }
+
+  try {
+    const savedPost = await postService.addPost(post, req.userToken!.id);
+    return res.status(201).send(savedPost);
+  } catch (error) {
+    // TODO: destroy the image if saving post fails?
+    return next(error);
+  }
+});
+
+// TODO: write a delete route
 
 export default router;
