c48f288574ef7852aa0eb10b5667a2260a664842
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-08 16:11:14 -0400

    Add more deugging print statements

--- backend/src/utils/cloudinary.ts
+++ backend/src/utils/cloudinary.ts
@@ -3,6 +3,10 @@ import config from './config';
 
 const cloudinary = Cloudinary.v2;
 
+console.log('name: ', config.CLOUDINARY_NAME);
+console.log('key: ', config.CLOUDINARY_API_KEY);
+console.log('secret: ', config.CLOUDINARY_API_SECRET);
+
 cloudinary.config({
   cloud_name: config.CLOUDINARY_NAME,
   api_key: config.CLOUDINARY_API_KEY,
