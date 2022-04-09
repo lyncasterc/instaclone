ab96323ea6c84c1895ef9061d8d44d101f3e82af
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-09 19:14:30 -0400

    Create put route for updating posts in posts router

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import postService from '../services/post-service';
 import { authenticator } from '../utils/middleware';
-import fieldParsers from '../utils/field-parsers';
+import fieldParsers, { parseStringField } from '../utils/field-parsers';
 import logger from '../utils/logger';
 import cloudinary from '../utils/cloudinary';
 import { User } from '../mongo';
@@ -41,6 +41,32 @@ router.post('/', authenticator(), async (req, res, next) => {
   } catch (error) {
     logger.error(logger.getErrorMessage(error));
     // TODO: destroy the image if saving post fails?
+    return next(error);
+  }
+});
+
+router.put('/:id', authenticator(), async (req, res, next) => {
+  const updatedPostFields = req.body;
+
+  try {
+    if (updatedPostFields.caption) updatedPostFields.caption = parseStringField(updatedPostFields.caption, 'caption');
+  } catch (error) {
+    logger.error(logger.getErrorMessage(error));
+    return res.status(400).send({ error: logger.getErrorMessage(error) });
+  }
+
+  try {
+    const updatedPost = await postService.updatePostById(
+      updatedPostFields,
+      req.params.id,
+      req.userToken!.id,
+    );
+    return res.status(200).send(updatedPost);
+  } catch (error) {
+    const errorMessage = logger.getErrorMessage(error);
+    logger.error(errorMessage);
+    if (errorMessage === 'Unauthorized') return res.status(401).send({ error: errorMessage });
+
     return next(error);
   }
 });
