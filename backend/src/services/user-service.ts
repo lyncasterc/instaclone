5d67f71fb6f78a7b8e2b5e2e410e3abc1e45c652
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-10 18:01:21 -0400

    Write followUserById in user service

--- backend/src/services/user-service.ts
+++ backend/src/services/user-service.ts
@@ -1,4 +1,5 @@
 import bcyrpt from 'bcrypt';
+import { ObjectId } from 'mongoose';
 import { User } from '../mongo';
 import { NewUser, ProofedUpdatedUser } from '../types';
 
@@ -44,6 +45,7 @@ const updateUserById = async (user: ProofedUpdatedUser, id: string) => {
     updatedUser.passwordHash = passwordHash;
   }
 
+  // FIXME: Pretty sure this can be in the if block above ^
   delete updatedUser.password;
 
   const savedUpdatedUser = await User.findByIdAndUpdate(id, updatedUser, { new: true })
@@ -52,9 +54,28 @@ const updateUserById = async (user: ProofedUpdatedUser, id: string) => {
   return savedUpdatedUser;
 };
 
+const followUserById = async (followerId: string, followedUserId: string) => {
+  const followedUser = await User.findById(followedUserId);
+  const follower = await User.findById(followerId);
+
+  if (!followedUser) throw new Error('User not found.');
+
+  // checking if user already follows the user they are requesting to follow
+  if (follower.following.map((ids: ObjectId) => ids?.toString()).includes(followedUser.id)) throw new Error('You already follow that user!');
+
+  followedUser.followers = [...followedUser.followers, follower];
+  follower.following = [...follower.following, followedUser.id];
+
+  await followedUser.save();
+  await follower.save();
+
+  return followedUser;
+};
+
 export default {
   getUsers,
   getUserById,
   addUser,
   updateUserById,
+  followUserById,
 };
