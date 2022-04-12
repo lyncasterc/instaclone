90fd2d54912ee124fb92ae4e0f2da72e797c920b
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-11 23:03:26 -0400

    Render GlobalStyles in App.tsx

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from '../common/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <p>Hello World</p>
+    </>
+  );
+}
+
+export default App;
