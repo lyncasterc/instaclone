9a3208adc04579282edd8ad064e4c40d8fb9b93e
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-16 02:03:55 -0400

    Create theme.ts for mantine theme object
    In seperate file so it can be used in App.tsx and for tests

---
+++ frontend/src/app/theme.ts
@@ -0,0 +1,10 @@
+import { MantineProviderProps } from '@mantine/core';
+
+const theme: MantineProviderProps['theme'] = {
+  colors: {
+    instaBlue: ['#B3CAD9', '#95BBD3', '#75AED2', '#51A3D9', '#289CE7', '#1681C7', '#0095F6', '#2471A3', '#2C6388', '#305872'],
+  },
+  primaryColor: 'instaBlue',
+};
+
+export default theme;
