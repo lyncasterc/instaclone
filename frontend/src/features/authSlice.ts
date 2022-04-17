61e2559058b6c0a65b196f8e15cdbc5b8ee9a897
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-16 21:57:33 -0400

    Create authSlice.ts to store current user's token information

---
+++ frontend/src/features/authSlice.ts
@@ -0,0 +1,47 @@
+import {
+  createSlice,
+  PayloadAction,
+  ThunkAction,
+  AnyAction,
+} from '@reduxjs/toolkit';
+import type { RootState } from '../app/store';
+
+export interface AuthState {
+  token: string | null,
+  username: string | null,
+}
+
+const initialState: AuthState = {
+  token: null,
+  username: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setAuthedUser: (
+      state,
+      { payload: { username, token } }: PayloadAction<{ username: string, token: string }>,
+    ) => {
+      state.username = username;
+      state.token = token;
+    },
+  },
+});
+
+export const { setAuthedUser } = authSlice.actions;
+
+export default authSlice.reducer;
+
+export const initAuthedUser = (): ThunkAction<void, RootState, unknown, AnyAction> => {
+  const token = localStorage.getItem('instacloneSCToken');
+  return (dispatch) => {
+    if (token) {
+      const parsedToken = JSON.parse(token);
+      dispatch(setAuthedUser(parsedToken));
+    }
+  };
+};
+
+export const selectCurrentUser = (state: RootState) => state.auth.username;
