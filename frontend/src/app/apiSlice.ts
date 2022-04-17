1acf08aca4cf6ba52f477147e9f3c4029492c98f
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-16 21:54:11 -0400

    Write inital apiSlice for all routes

---
+++ frontend/src/app/apiSlice.ts
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { User, NewUserFields, LoginFields } from './types';
+import type { AuthState } from '../features/authSlice';
+
+export const apiSlice = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api',
+  }),
+  tagTypes: ['User', 'Post'],
+  endpoints: (builder) => ({
+    addUser: builder.mutation<User, NewUserFields>({
+      query: (newUserFields) => ({
+        url: '/users',
+        method: 'POST',
+        body: newUserFields,
+      }),
+      // invalidatesTags: ['User'],
+    }),
+    login: builder.mutation<AuthState, LoginFields>({
+      query: (loginFields) => ({
+        url: '/login',
+        method: 'POST',
+        body: loginFields,
+      }),
+    }),
+  }),
+});
+
+export const { useAddUserMutation, useLoginMutation } = apiSlice;
