fc116cb4d063f6b8144685b3f7a16620b095cc9c
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-18 15:28:04 -0400

    Write RequiredAuth for protected routes

---
+++ frontend/src/features/auth/RequireAuth.tsx
@@ -0,0 +1,12 @@
+import { useLocation, Navigate } from 'react-router-dom';
+import { ReactElement } from 'react';
+import useAuth from '../../common/hooks/useAuth';
+
+function RequireAuth({ children }: { children: ReactElement }) {
+  const [user] = useAuth();
+
+  const location = useLocation();
+  return user ? (children) : (<Navigate to="/login" state={{ path: location.pathname }} />);
+}
+
+export default RequireAuth;
