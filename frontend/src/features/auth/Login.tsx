9591c9fcef8fa3325c41183bf6560180dcb111ef
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-19 18:46:33 -0400

    Write complete onSubmit for the Login page

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -1,5 +1,6 @@
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+import { useState } from 'react';
 import {
   Container,
   Title,
@@ -7,12 +8,17 @@ import {
   Text,
   createStyles,
 } from '@mantine/core';
-import { Link } from 'react-router-dom';
+import {
+  Link,
+  useNavigate,
+  useLocation,
+} from 'react-router-dom';
 import type { LoginFields } from '../../app/types';
 import FormikTextInput from '../../common/components/FormikTextInput';
 import Button from '../../common/components/Button';
 import FormContainer from '../../common/components/FormContainer';
 import useAuth from '../../common/hooks/useAuth';
+import getErrorMessage from '../../common/utils/getErrorMessage';
 
 // exported for use in SignUp.tsx, which follows the same structure and design.
 export const useStyles = createStyles(() => ({
@@ -25,10 +31,18 @@ export const useStyles = createStyles(() => ({
   },
 }));
 
+interface LocationState {
+  path: string,
+}
+
 // TODO: redirect user to feed if already logged in
 function Login() {
+  const [errorMessage, setErrorMessage] = useState('');
   const [, { login }] = useAuth();
   const { classes } = useStyles();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const state = location.state as LocationState;
   return (
 
     <Container
@@ -40,8 +54,14 @@ function Login() {
           password: '',
         }}
         onSubmit={async (values: LoginFields) => {
-          console.log('login boop');
-          await login(values);
+          try {
+            await login(values);
+            navigate(state?.path || '/');
+          } catch (error) {
+            const message = getErrorMessage(error);
+            console.error(message);
+            setErrorMessage(message);
+          }
         }}
         validationSchema={Yup.object({
           username: Yup.string().required(),
@@ -88,6 +108,10 @@ function Login() {
                 disabled={!dirty || !isValid}
                 style={{ marginTop: 10 }}
               />
+
+              {
+                errorMessage && <Text sx={{ color: 'red', marginTop: 10 }}>{errorMessage}</Text>
+              }
 
               <Text sx={{ marginTop: 10 }}>
                 Don&apos;t have an account?
