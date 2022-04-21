431bef911e348be8ed66af71cfe0f68777b35746
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-20 21:34:44 -0400

    Write tests for mockLogin

---
+++ frontend/src/test/utils/mockLogin.test.ts
@@ -0,0 +1,29 @@
+import { mockLogin } from './test-utils';
+import { store } from '../../app/store';
+import { removeCurrentUser } from '../../features/auth/authSlice';
+
+afterEach(() => {
+  localStorage.removeItem('instacloneSCToken');
+  store.dispatch(removeCurrentUser());
+});
+
+const fakeTokenInfo = {
+  username: 'bobbydob',
+  token: 'supersecrettoken',
+};
+
+test('mockLogin saves a token in localStorage', () => {
+  mockLogin({ fakeTokenInfo });
+  const storedToken = localStorage.getItem('instacloneSCToken');
+
+  expect(storedToken).not.toBeNull();
+  expect(JSON.parse(storedToken!)).toEqual(fakeTokenInfo);
+});
+
+test('mockLogin stores the token info in the Redux store', () => {
+  mockLogin({ fakeTokenInfo });
+  const state = store.getState();
+
+  expect(state.auth.username).toBe(fakeTokenInfo.username);
+  expect(state.auth.token).toBe(fakeTokenInfo.token);
+});
