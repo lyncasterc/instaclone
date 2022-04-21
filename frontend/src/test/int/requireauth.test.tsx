783f1da193d12ab246f2d545ce862ea8dd374d68
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-20 21:36:08 -0400

    Refactor

--- frontend/src/test/int/requireauth.test.tsx
+++ frontend/src/test/int/requireauth.test.tsx
@@ -1,37 +1,28 @@
-import { MemoryRouter } from 'react-router-dom';
 import {
   screen,
-  render,
-} from '../../common/utils/test-utils';
+  renderWithRouter,
+  mockLogin,
+} from '../utils/test-utils';
 import App from '../../app/App';
-import { store } from '../../app/store';
-import { initAuthedUser } from '../../features/auth/authSlice';
 import '@testing-library/jest-dom/extend-expect';
 
 test('redirects user to login form when not logged in', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
+  renderWithRouter(
+    <App />,
   );
 
   expect(screen.getByText(/log in/i)).toBeVisible();
 });
 
 test('logged in user is taken to home page', async () => {
-  // mocking logged in state
   const fakeTokenInfo = {
     username: 'bobbydob',
     token: 'supersecrettoken',
   };
-  localStorage.setItem('instacloneSCToken', JSON.stringify(fakeTokenInfo));
+  mockLogin({ fakeTokenInfo });
 
-  // mirroring the logic in index.tsx
-  store.dispatch(initAuthedUser());
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
+  renderWithRouter(
+    <App />,
   );
 
   // TODO: update this when home page is nearing completion
