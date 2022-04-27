5a8e935d3e9bf2268907eed99e231568bbfc5287
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-27 14:40:23 -0400

    Create BottomNavbar for mobile

---
+++ frontend/src/common/components/BottomNavBar/BottomNavbar.styles.ts
@@ -0,0 +1,15 @@
+import { createStyles } from '@mantine/core';
+
+export default createStyles((theme) => ({
+  container: {
+    // display: 'flex',
+    // justifyContent: 'space-between',
+    // alignItems: 'center',
+    backgroundColor: 'white',
+    width: '100%',
+    padding: '5px 20px',
+    position: 'fixed',
+    bottom: 0,
+    borderTop: `1px solid ${theme.colors.gray[4]}`,
+  },
+}));
