fa22d23190738573d99f580737c0fba835ef7684
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-27 16:48:16 -0400

    Create navbar for home page

---
+++ frontend/src/features/users/Home/HomeNavbar/HomeNavbar.styles.ts
@@ -0,0 +1,12 @@
+import { createStyles } from '@mantine/core';
+
+export default createStyles(() => ({
+  navBrandTitle: {
+    fontSize: '2rem',
+    fontFamily: 'Damion',
+    color: 'black',
+  },
+  navBrandLink: {
+    textDecoration: 'none',
+  },
+}));
