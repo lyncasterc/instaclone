a176c7b008d14406c66f5f120d00d5ebc3c1542c
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-27 17:44:56 -0400

    Delete desktop navbar tests
    The tests are no longer runnable now that the component has media queries

--- frontend/src/test/int/desktopnavbar.int.test.tsx
+++
@@ -1,5 +0,0 @@
-// import { screen, renderWithRouter } from '../utils/test-utils';
-// import '@testing-library/jest-dom/extend-expect';
-
-// TODO: int - That clicking navbar brand takes you to home
-export default {};
