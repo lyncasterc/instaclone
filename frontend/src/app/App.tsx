b093dd3e9e11a9525ec3e70c2108c49b2921e89b
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-04-27 20:02:44 -0400

    Add logic to not render navs in login or signup page

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,19 +1,34 @@
-// import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+// import { Container } from '@mantine/core';
 import Damion from '../common/components/Damion';
 import GlobalStyles from '../common/components/GlobalStyles';
 import Login from '../features/auth/Login';
 import SignUp from '../features/users/SignUp';
-import Home from '../features/users/Home';
+import Home from '../features/users/Home/Home';
 import RequireAuth from '../features/auth/RequireAuth';
 import useAuth from '../common/hooks/useAuth';
+import DesktopNavbar from '../common/components/DesktopNavbar/DesktopNavbar';
+import BottomNavBar from '../common/components/BottomNavBar/BottomNavbar';
 
 function App() {
   const [user] = useAuth();
+  const location = useLocation();
   return (
     <>
       <GlobalStyles />
       <Damion />
+      {
+        !(/\/signup|\/login/.test(location.pathname)) && (
+          <>
+            <DesktopNavbar />
+            <BottomNavBar />
+          </>
+        )
+      }
 
       <Routes>
         <Route
