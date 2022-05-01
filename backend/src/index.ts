d15e89b73513f4fa78063c54a4ebdc6bba8787e3
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-01 16:12:09 -0400

    Run backend on 3001 if PORT env is undefined

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,6 @@ import config from './utils/config';
 
 const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
+server.listen(config.PORT || 3001, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
