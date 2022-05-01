2eb2da61c4b496c7207b717dcdb14bb67ae66926
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-01 16:18:09 -0400

    Make port variable for backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,8 @@ import logger from './utils/logger';
 import config from './utils/config';
 
 const server = http.createServer(app);
+const port = config.PORT || 3001;
 
-server.listen(config.PORT || 3001, () => {
-  logger.info(`Server running on port ${config.PORT}`);
+server.listen(port, () => {
+  logger.info(`Server running on port ${port}`);
 });
