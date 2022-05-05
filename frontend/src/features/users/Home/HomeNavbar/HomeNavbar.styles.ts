90efde7bdd4f8b0b09a60bdd67b07d4f10b63498
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-04 21:44:34 -0400

    Refactor to use NavbarBrand

--- frontend/src/features/users/Home/HomeNavbar/HomeNavbar.styles.ts
+++ frontend/src/features/users/Home/HomeNavbar/HomeNavbar.styles.ts
@@ -1,12 +1,5 @@
 import { createStyles } from '@mantine/core';
 
 export default createStyles(() => ({
-  navBrandTitle: {
-    fontSize: '2rem',
-    fontFamily: 'Damion',
-    color: 'black',
-  },
-  navBrandLink: {
-    textDecoration: 'none',
-  },
+
 }));
