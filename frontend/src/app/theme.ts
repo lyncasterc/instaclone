dc308d5a5aba7b5c57ffd89f133665ca59ea7cdb
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-05 18:28:56 -0400

    Add new custom color

--- frontend/src/app/theme.ts
+++ frontend/src/app/theme.ts
@@ -3,6 +3,7 @@ import { MantineProviderProps } from '@mantine/core';
 const theme: MantineProviderProps['theme'] = {
   colors: {
     instaBlue: ['#B3CAD9', '#95BBD3', '#75AED2', '#51A3D9', '#289CE7', '#1681C7', '#0095F6', '#2471A3', '#2C6388', '#305872'],
+    instaDark: ['#434343', '#3D3D3D', '#383838', '#333333', '#2E2E2E', '#2A2A2A', '#262626', '#222222', '#1F1F1F', '#1C1C1C'],
   },
   primaryColor: 'instaBlue',
 };
