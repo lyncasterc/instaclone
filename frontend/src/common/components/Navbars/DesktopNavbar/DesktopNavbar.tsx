2f936a75e6438bf9d2079d09c8568f75fa5ab906
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-04 21:46:13 -0400

    Move to /Navbars, refactor to use NavbarBrand

---
+++ frontend/src/common/components/Navbars/DesktopNavbar/DesktopNavbar.tsx
@@ -0,0 +1,97 @@
+import { Button } from '@mantine/core';
+import {
+  Home,
+  SquarePlus,
+} from 'tabler-icons-react';
+import { Link } from 'react-router-dom';
+import useStyles from './DesktopNavbar.styles';
+import useAuth from '../../../hooks/useAuth';
+import UserMenu from '../../UserMenu/UserMenu';
+import NavbarBrand from '../NavbarBrand/NavbarBrand';
+
+interface DesktopNavbarProps {
+  displayOnMobile?: boolean,
+}
+
+function DesktopNavbar({ displayOnMobile }: DesktopNavbarProps) {
+  const [user] = useAuth();
+  const { classes, cx } = useStyles();
+
+  const rightNavSection = () => (
+    <div
+      className={classes.navBarButtonGroup}
+    >
+      {
+        (!user ? (
+          <>
+            <Button
+              component={Link}
+              to="/login"
+              classNames={{
+                root: classes.buttonRoot,
+              }}
+            >
+              Log In
+            </Button>
+            <Button
+              variant="white"
+              component={Link}
+              to="/signup"
+              classNames={{
+                root: cx(classes.buttonRoot, classes.signupButtonRoot),
+              }}
+            >
+              Sign Up
+            </Button>
+          </>
+        ) : (
+          <>
+            <Link
+              to="/"
+            >
+              <Home
+                size={30}
+                strokeWidth={2}
+                color="black"
+              />
+            </Link>
+            <Link
+              to="/"
+            >
+              <SquarePlus
+                size={30}
+                strokeWidth={2}
+                color="black"
+              />
+            </Link>
+            <UserMenu />
+          </>
+
+        ))
+      }
+    </div>
+  );
+
+  return (
+    <nav
+      className={cx(classes.navContainer, { [classes.hideOnMobile]: !displayOnMobile })}
+      data-cy="desktop-nav"
+    >
+      <div>
+        <NavbarBrand />
+      </div>
+
+      <div>
+        <div>
+          {rightNavSection()}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+DesktopNavbar.defaultProps = {
+  displayOnMobile: false,
+};
+
+export default DesktopNavbar;
