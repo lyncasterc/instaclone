0b155ff3dc97f292b2ade0683c49600182ef3ab6
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-05 22:36:25 -0400

    Start UserProfile page

---
+++ frontend/src/features/users/UserProfile/UserProfile.tsx
@@ -0,0 +1,56 @@
+import { useParams } from 'react-router-dom';
+import { useMediaQuery } from '@mantine/hooks';
+import UserProfileNavbar from './UserProfileNavbar/UserProfileNavbar';
+import UserProfileInfo from './UserProfileInfo';
+import UserProfileInfoBar from './UserProfileInfoBar/UserProfileInfoBar';
+import DesktopNavbar from '../../../common/components/Navbars/DesktopNavbar/DesktopNavbar';
+import useAuth from '../../../common/hooks/useAuth';
+
+function UserProfile() {
+  const { username } = useParams();
+  const [user] = useAuth();
+  const isMediumScreenOrWider = useMediaQuery('(min-width: 1000px)');
+
+  const isCurrentUserProfile = user !== null && user === username;
+  // TODO: add logic for verifying that username exists
+
+  const fakeUser = {
+    id: '1',
+    username: username ?? 'bob',
+    fullName: 'test',
+    email: 'test',
+    passwordHash: 'test',
+  };
+
+  return (
+    <>
+      {
+      user ? (
+        <UserProfileNavbar
+          username={username as string}
+          isCurrentUserProfile={isCurrentUserProfile}
+        />
+      ) : (
+        <DesktopNavbar displayOnMobile />
+      )
+    }
+      <UserProfileInfo
+        user={fakeUser}
+        isCurrentUserProfile={isCurrentUserProfile}
+        isCurrentUserLoggedIn={user === null}
+      />
+      {
+        !isMediumScreenOrWider && (
+          <UserProfileInfoBar
+            postCount={1000}
+            followerCount={13560}
+            followingCount={467003}
+          />
+        )
+      }
+
+    </>
+  );
+}
+
+export default UserProfile;
