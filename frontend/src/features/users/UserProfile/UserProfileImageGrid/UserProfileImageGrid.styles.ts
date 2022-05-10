92f669e51c2bb88f82a25fae8ca5c8fbf6314e6f
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-10 19:08:33 -0400

    Add more gap on larger screens

--- frontend/src/features/users/UserProfile/UserProfileImageGrid/UserProfileImageGrid.styles.ts
+++ frontend/src/features/users/UserProfile/UserProfileImageGrid/UserProfileImageGrid.styles.ts
@@ -1,10 +1,14 @@
 import { createStyles } from '@mantine/core';
 
-export default createStyles(() => ({
+export default createStyles((theme) => ({
   container: {
 
   },
   grid: {
     gap: 3,
+
+    [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+      gap: 28,
+    },
   },
 }));
