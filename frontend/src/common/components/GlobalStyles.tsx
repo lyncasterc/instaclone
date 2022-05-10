767c9eae3cadc88c2e71f7cdb9e0685c4716caeb
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-10 19:09:18 -0400

    Add global style to help with CSS debugging

--- frontend/src/common/components/GlobalStyles.tsx
+++ frontend/src/common/components/GlobalStyles.tsx
@@ -4,6 +4,11 @@ function GlobalStyles() {
   return (
     <Global
       styles={(theme) => ({
+        // CSS debugging
+        // '*': {
+        //   border: '1px solid red',
+        // },
+
         '*, *::before, *::after': {
           boxSizing: 'border-box',
         },
