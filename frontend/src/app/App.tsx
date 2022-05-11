3d440affa0e58bfb2d8cf410b55cd2db6786c6ef
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-10 21:54:29 -0400

    Pass down user to BottomNavbar

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -25,7 +25,7 @@ function App() {
         user && (
           <>
             <DesktopNavbar />
-            <BottomNavBar />
+            <BottomNavBar user={user} />
           </>
         )
       }
