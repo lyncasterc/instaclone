6b751bd19eebde421c6aaf64fab1903f06700926
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-05-12 22:52:15 -0400

    Make UserProfile dynamic

--- frontend/src/features/users/UserProfile/UserProfile.tsx
+++ frontend/src/features/users/UserProfile/UserProfile.tsx
@@ -6,53 +6,58 @@ import UserProfileInfoBar from './UserProfileInfoBar/UserProfileInfoBar';
 import UserProfileImageGrid from './UserProfileImageGrid/UserProfileImageGrid';
 import DesktopNavbar from '../../../common/components/Navbars/DesktopNavbar/DesktopNavbar';
 import useAuth from '../../../common/hooks/useAuth';
+import { selectUserByUsername } from '../../../app/apiSlice';
+import { useAppSelector } from '../../../common/hooks/selector-dispatch-hooks';
 
 function UserProfile() {
   const { username } = useParams();
   const [user] = useAuth();
+  const selectedUser = username ? useAppSelector(
+    (state) => selectUserByUsername(state, username),
+  ) : undefined;
   const isMediumScreenOrWider = useMediaQuery('(min-width: 992px)');
 
   const isCurrentUserProfile = user !== null && user === username;
   // TODO: add logic for verifying that username exists
   // and then remove type assertion from line 31
 
-  const fakeUser = {
-    id: '1',
-    username: username ?? 'bob',
-    fullName: 'test',
-    email: 'test',
-    passwordHash: 'test',
-  };
-
-  return (
-    <>
-      {
-      user ? (
-        <UserProfileNavbar
-          username={username as string}
-          isCurrentUserProfile={isCurrentUserProfile}
-        />
-      ) : (
-        <DesktopNavbar displayOnMobile />
-      )
-    }
-      <UserProfileInfo
-        user={fakeUser}
-        isCurrentUserProfile={isCurrentUserProfile}
-        isCurrentUserLoggedIn={user === null}
-      />
-      {
-        !isMediumScreenOrWider && (
-          <UserProfileInfoBar
-            postCount={1000}
-            followerCount={13560}
-            followingCount={467003}
+  if (selectedUser) {
+    return (
+      <>
+        {
+        user ? (
+          <UserProfileNavbar
+            username={username as string}
+            isCurrentUserProfile={isCurrentUserProfile}
           />
+        ) : (
+          <DesktopNavbar displayOnMobile />
         )
       }
+        <UserProfileInfo
+          user={selectedUser}
+          isCurrentUserProfile={isCurrentUserProfile}
+          isCurrentUserLoggedIn={user === null}
+        />
+        {
+          !isMediumScreenOrWider && (
+            <UserProfileInfoBar
+              postCount={selectedUser.posts?.length ?? 0}
+              followerCount={selectedUser.followers?.length ?? 0}
+              followingCount={selectedUser.following?.length ?? 0}
+            />
+          )
+        }
 
       <UserProfileImageGrid />
 
+      </>
+    );
+  }
+
+  return (
+    <>
+      Sorry, this page isn&apos;t available.
     </>
   );
 }
